c1f1655d810a6fcec9ef7c1e529874d333251095
asif93-138 <asif93@student.sust.edu>
2023-09-19 09:28:28 +0600

    test deployment..

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express')
+var cors = require('cors')
+const jobs = require('./jobs.json')
+const app = express()
+const port = 3000
+
+app.use(cors())
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/data', (req, res) => {
+    res.send('Data loading !!!')
+  })
+
+
+app.get('/jobs', (req, res) => {
+    res.send(jobs)
+})
+
+
+app.get('/jobs/:id', (req, res) => {
+    const id = Number(req.params.id);
+    console.log('id : ', id);
+    const job = jobs.find(job => job.id === id) || {};
+    res.send(job)
+  })
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
